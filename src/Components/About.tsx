e483d4c980b22f0885034443d1e7a567f4c47890
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-06-26 19:24:34 +0200

    feat: video inside About component

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,12 +1,20 @@
 import styles from "../assets/styles/About.module.css"
 import { translate } from "../utils/common"
-import { useContext } from "react"
+import { useContext, useEffect, useRef } from "react"
 import { LanguageContext } from "../utils/context/LanguageContext"
 import { LanguageContextType } from "../utils/types/context"
-
+import clip from "../assets/videos/clip.mp4"
 
 function About() {
     const {lang} = useContext(LanguageContext) as LanguageContextType
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            console.log(videoRef.current)
+            videoRef.current.volume = 0.2;
+        }
+    }, []);
     
     return(
         <div id='about-me' className={styles.mainContainer}>
@@ -16,7 +24,9 @@ function About() {
                 
                 <div className={styles.leftPart}>
                     <h2 className="subtitle">{translate(lang).main.about.subtitle}</h2>
-                    <div className={styles.video}></div>
+                    <video ref={videoRef} className={styles.video} controls>
+                        <source src={clip} type="video/mp4" />
+                    </video>
                 </div>
                 <div className={styles.rightPart}>
                     <div>
